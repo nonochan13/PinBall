febe0a003ba8dbc3a112c1e1ef3987f01f35b1d9
nonochan13 <ueno.jit@gmail.com>
2018-07-03 23:04:01 +0900

    PinBallの課題です。 発展課題はキャンセルし、Lesson6に進みます。

---
+++ Assets/Score.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour {
+    public GameObject scoreText; //Text用変数
+    private int score = 0; //スコア計算用変数
+
+
+	// Use this for initialization
+	void Start () 
+        {
+            //シーン中のGameOverTextオブジェクトを取得
+            this.scoreText = GameObject.Find("ScoreText");
+            score = 0;
+            SetScore(); //初期スコアを代入して表示
+        }
+
+// 小さい星は+10点、大きい星は+20点、小さい雲は+5点、大きい雲は+１点
+void OnCollisionEnter( Collision collision ) {
+        string Ball = collision.gameObject.tag;
+        if (Ball == "SmallStarTag")
+        {
+            score += 10;
+        }
+    else if (Ball == "LargeStarTag")
+        {
+            score += 20;
+        }
+    else if (Ball == "SmallCloudTag")
+        {
+            score += 5;
+        }
+    else if (Ball == "LargeCloudTag")
+        {
+            score += 1;
+        }
+        SetScore();
+    }
+    void SetScore()
+    {
+        scoreText.GetComponent<Text>().text = string.Format("Score:{0}", score);
+    }
+}
